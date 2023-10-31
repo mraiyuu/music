0ecd5016500bbcca46f8538f887d0870786b4cfe
mraiyuuu <munenedenis618@gmail.com>
2023-10-31 16:51:33 +0300

    assets

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import facebookLogo from './facebookLogo.svg';
+import frame4 from './frame4.svg';
+import group_7 from './group_7.svg';
+import header from './header.png';
+import instagramLogo from './instagramLogo.svg';
+import menuIcon from './menuIcon.svg';
+import rectangle_19_2 from './rectangle_19_2.png';
+import reactangle_19_3 from './reactangle_19_3.png';
+import rectangle_19 from './rectangle_19.png';
+import students from './students.png';
+import studentH from './studentH.png';
+import thumbnail_2 from './thumbnail_2.png';
+import thumbnail_3 from './thumbnail_3.png';
+import thumbnail from './thumbnail.png';
+import twitterLogo from './twitterLogo.svg';
+
+export { facebookLogo, frame4, group_7, header, instagramLogo, menuIcon, rectangle_19_2, reactangle_19_3, rectangle_19, students, studentH, thumbnail_2, thumbnail_3, thumbnail, twitterLogo };
