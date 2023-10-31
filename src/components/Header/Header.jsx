e92209cb9f77fc60977bbb0e910518ee257e4ba4
mraiyuuu <munenedenis618@gmail.com>
2023-10-31 22:53:31 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header>
       <div className="wrapper">
         <div className="cta">
-        <p classsName="course-name">Sound design Masterclass</p>
+        <p className="course-name">Sound Design Masterclass</p>
         <h1>Learn the art of sound design</h1>
         <a href="#" className="demo-btn">Demo lesson</a>
         </div>
