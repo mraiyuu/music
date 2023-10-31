e1fbea262a866b31a885254c0d06e8667e383c04
mraiyuuu <munenedenis618@gmail.com>
2023-10-31 16:03:55 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Header, Topics, Info, Blog, Testimonies, Footer } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Topics />
+      <Info />
+      <Blog />
+      <Testimonies />
+      <Footer />
+      
+    </>
+  );
+};
+
+export default App;
