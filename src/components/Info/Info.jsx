00b4dc824fee25eb8e134cd06d81a995103c9f9f
mraiyuuu <munenedenis618@gmail.com>
2023-11-01 12:27:12 +0300

    info almost don

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import './Info.css';
+import React from "react";
+import "./Info.css";
+import { student, hours, group_7 } from "../../assets";
 
 const Info = () => {
   return (
-    <div>Info</div>
-  )
-}
+    <section id="info" className="dark-gray">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="info-content">
+            <img src={student} />
+            <div className="amount">23,000+</div>
+            <div className="type">Students</div>
+          </div>
 
-export default Info
+          <div className="info-content">
+            <img src={hours} />
+            <div className="amount">26 Hrs</div>
+            <div className="type">Video Content</div>
+          </div>
+         
+        </div>
+        <img className="bg-element-2" src={group_7} />
+       
+      </div>
+    </section>
+  );
+};
+
+export default Info;
