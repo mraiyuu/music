886627e48ba18d17d8a50ee2e7b4ca76fddb9d17
mraiyuuu <munenedenis618@gmail.com>
2023-11-01 09:56:02 +0300

    sections almost done

--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -15,7 +15,7 @@ const Topics = () => {
   const [currentImage, setCurrentImage] = useState(frequencies);
 
   return (
-    <section id="section" className="black">
+    <section id="topics" className="black">
       <div className="wrapper">
         <h2>What will you learn?</h2>
         <div className="content-container">
@@ -24,8 +24,8 @@ const Topics = () => {
             <li onMouseEnter={() => setCurrentImage(daw)}>Using DAW</li>
             <li onMouseEnter={() => setCurrentImage(vocalProcessing)}>Vocal processing</li>
             <li onMouseEnter={() => setCurrentImage(mixing)}>Mixing</li>
-            <li>Mixing console</li>
-            <li>Mastering</li>
+            <li onMouseEnter={() => setCurrentImage(mixingConsole)}>Mixing console</li>
+            <li onMouseEnter={() => setCurrentImage(mastering)}>Mastering</li>
           </ul>
           <div className="topic-image">
             <img src={currentImage}></img>
