5d735213116d43e218c5be00bdbaa66e7d9c717f
mraiyuuu <munenedenis618@gmail.com>
2023-11-02 09:38:29 +0300

    footer almost done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 import './Footer.css';
+import { facebookLogo, twitterLogo, instagramLogo } from '../../assets';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="black">
+      <div className="content-container">
+        <div className="links">
+          <a href="#" className="logo">
+            Sound<span className="red">SZign</span>
+          </a>
+          <div className="social-icon">
+            <a href="#">
+              <img src={facebookLogo}/>
+            </a>
+            <a href="#">
+              <img src={twitterLogo}/>
+            </a>
+            <a href="#">
+              <img src={instagramLogo}/>
+            </a>
+            <div className="copy-right">
+            This website is designed by Mraiyuu Ⓒ 2023
+            </div>
+
+          </div>
+          <div className="quick-links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+          <div className="links">
+            <h3>Contact Us</h3>
+            <ul>
+              <li>
+                <a href="#">
+                  munenedenis618@gmail.com
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  +254715316525
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
